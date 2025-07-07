b6c6d671cf5f219d09a90835d6f96efcf45df1e0
revprm <revy.pramana@gmail.com>
2025-07-07 12:27:56 +0700

    Refactor code formatting in config.py and main.py for improved readability

--- src/app/main.py
+++ src/app/main.py
@@ -5,14 +5,17 @@ import uvicorn
 from pydantic import BaseModel
 from config.config import config
 
+
 class IrisInput(BaseModel):
     sepal_length: float
     sepal_width: float
     petal_length: float
     petal_width: float
 
+
 app = FastAPI()
 model = joblib.load(config["train"]["model_path"])
+
 
 @app.post("/predict")
 def predict(data: IrisInput):
@@ -20,5 +23,10 @@ def predict(data: IrisInput):
     prediction = model.predict(df)
     return {"prediction": int(prediction[0])}
 
+
 if __name__ == "__main__":
-    uvicorn.run(app, host=config["deployment"]["app_host"], port=config["deployment"]["app_port"])
+    uvicorn.run(
+        app,
+        host=config["deployment"]["app_host"],
+        port=config["deployment"]["app_port"],
+    )
